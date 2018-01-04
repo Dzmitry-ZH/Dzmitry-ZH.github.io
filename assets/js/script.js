ad8e06e7d0489e38a0b8bb4ad43fd25906e0c712
Dzmitry Zhyshkevich <dimach_96@mail.ru>
2018-01-05 02:27:32 +0300

    first page

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $preloader.delay(500).fadeOut('slow');
+});
+
+new WOW().init();
