175690f03509df755428ca7b2e1cbe8774a889ea
Dzmitry Zhyshkevich <dimach_96@mail.ru>
2018-01-06 01:37:48 +0300

    Finished first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,23 @@ $(window).on('load', function () {
 
 new WOW().init();
 
+$(document).ready(function() {
+    $('.text .text-block').eq(0).addClass('active').fadeIn(1000);
+
+    setInterval('blockAnimate();', 8000);
+});
+
+
+function blockAnimate() {
+    var length = $('.text .text-block').length - 1;
+    $('.text .text-block').each(function(index) {
+        if($(this).hasClass('active') && index != length) {
+            $(this).removeClass('active').fadeOut(1000).next('.text-block').addClass('active').delay(1000).fadeIn(1000);
+            return false;
+        } else if (index == length) {
+            $(this).removeClass('active').fadeOut(1000);
+            $('.text .text-block').eq(0).addClass('active').delay(1000).fadeIn(1000);
+            return false;
+        }
+    });
+};
