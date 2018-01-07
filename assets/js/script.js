e635a7f4c544c37af25d15d46f2ef29dc3ffd117
Dzmitry Zhyshkevich <dimach_96@mail.ru>
2018-01-08 00:26:46 +0300

    added about us page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,38 @@ $(function () {
     $('#main').on('click', function () {
         show($("main"));
         hide($(".service"));
+        hide($('.about-us'));
     });
 
     $('#service').on('click', function () {
         show($(".service"));
         hide($("main"));
+        hide($('.about-us'));
 
+    });
+
+    $('#about-us').on('click', function () {
+        show($('.about-us'));
+        hide($('main'));
+        hide($('.service'));
     });
 
     $('#main1').on('click', function () {
         show($("main"));
         hide($(".service"));
+        hide($('.about-us'));
     });
 
     $('#service1').on('click', function () {
         show($(".service"));
         hide($("main"));
+        hide($('.about-us'));
+
+        $('#about-us1').on('click', function () {
+            show($('.about-us'));
+            hide($('main'));
+            hide($('.service'));
+        });
 
     });
 
@@ -44,7 +60,7 @@ $(function () {
 
 new WOW().init();
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.text .text-block').eq(0).addClass('active').fadeIn(1000);
 
     setInterval('blockAnimate();', 8000);
@@ -53,8 +69,8 @@ $(document).ready(function() {
 
 function blockAnimate() {
     var length = $('.text .text-block').length - 1;
-    $('.text .text-block').each(function(index) {
-        if($(this).hasClass('active') && index != length) {
+    $('.text .text-block').each(function (index) {
+        if ($(this).hasClass('active') && index != length) {
             $(this).removeClass('active').fadeOut(1000).next('.text-block').addClass('active').delay(1000).fadeIn(1000);
             return false;
         } else if (index == length) {
