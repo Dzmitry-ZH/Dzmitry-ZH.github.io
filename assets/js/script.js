87fdbf50cf50a3dcd2d28a200cc58acac17750eb
Dzmitry Zhyshkevich <dimach_96@mail.ru>
2018-01-08 08:56:47 +0300

    disable wow for mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,15 @@ $(function () {
 });
 
 
-new WOW().init();
+var wow = new WOW(
+    {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       false       // trigger animations on mobile devices (true is default)
+    }
+);
+wow.init();
 
 $(document).ready(function () {
     $('.text .text-block').eq(0).addClass('active').fadeIn(1000);
